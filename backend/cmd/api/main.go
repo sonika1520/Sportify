31c7f1e12137909ec01f15c079b34f103a7faa03
abdulkalam556 <shaikabdulkalamazad1@gmail.com>
2025-02-07 15:03:33 -0500

    added all the intial setup that required for the backend of the project

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/MishNia/Sportify.git/internal/env"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+	log.Fatal(app.run(mux))
+}
---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/MishNia/Sportify.git
+
+go 1.23.6
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
